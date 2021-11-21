9f1e435572a092aae7c2e6d4d0c1ee52042edc42
CalenShirfield <calenshirfield@gmail.com>
2021-11-21 22:55:37 +0000

    Update useStack.js

--- stack/useStack.js
+++ stack/useStack.js
@@ -1,9 +1,11 @@
-let ss = require("./Stack");
+let s = require("./Stack");
 
-let tempStack = new ss.Stack();
+let tempStack = new s.Stack();
 tempStack.push(10);
 tempStack.push(300);
 tempStack.push(56);
 
-// console.log(tempStack.top());
+console.log(tempStack.top());
+console.log(tempStack.pop());
+console.log(tempStack.length());
 console.log(tempStack.show());
