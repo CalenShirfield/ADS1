1fbd2449b8783fa11e0f1f35c69029f56ed9c2a5
CalenShirfield <calenshirfield@gmail.com>
2021-11-22 16:09:28 +0000

    updated stack show method

--- stack/Stack.js
+++ stack/Stack.js
@@ -1,40 +1,40 @@
 class Stack {
   constructor() {
-    this.pile = [];
+    this.s = [];
   }
   // push function array method
   push(element) {
-    this.pile.push(element);
+    this.s.push(element);
   }
   // pop function utilizing array method
   pop(element) {
-    if (this.pile.length == 0) {
+    if (this.s.length == 0) {
       return "underflow!";
     } else {
-      return this.pile.pop();
+      return this.s.pop();
     }
   }
   // length function array prototype property
   length() {
-    return this.pile.length;
+    return this.s.length;
   }
   // top function to reveal top of stack without removing element
   top() {
-    return this.pile[this.pile.length - 1];
+    return this.s[this.s.length - 1];
   }
   // isEmpty function
   isEmpty() {
-    if (this.pile.length == 0) {
+    if (this.s.length == 0) {
       return true;
     } else {
       return false;
     }
   }
-  // show function lists all elements of stack as a string
+  // show function lists elements vertically in order aa a string
   show() {
     let list = "";
-    for (let i = 0; i < this.pile.length; i++) {
-      list += this.pile[i] + " ";
+    for (let i = this.s.length - 1; i > -1; i--) {
+      list += this.s[i] + "\n";
     }
     return list;
   }
--- stack/testStack.js
+++ stack/testStack.js
@@ -3,9 +3,14 @@ let s = require("./Stack");
 let tempStack = new s.Stack();
 tempStack.push(10);
 tempStack.push(300);
+tempStack.push(73);
 tempStack.push(56);
+tempStack.push(11);
+tempStack.push(400);
 
 console.log(tempStack.top());
-console.log(tempStack.pop());
+// console.log(tempStack.pop());
 console.log(tempStack.length());
+
+console.log("");
 console.log(tempStack.show());
